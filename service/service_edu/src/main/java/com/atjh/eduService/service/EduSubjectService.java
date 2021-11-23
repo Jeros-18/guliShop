182c1b943843b4f263700c8884001ffc12cd00de
JiaHui <1474352803@qq.com>
2021-11-23 20:11:08 +0800

    讲师模块

---
+++ service/service_edu/src/main/java/com/atjh/eduService/service/EduSubjectService.java
@@ -0,0 +1,16 @@
+package com.atjh.eduService.service;
+
+import com.atjh.eduService.entity.EduSubject;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程科目 服务类
+ * </p>
+ *
+ * @author jahui
+ * @since 2021-11-23
+ */
+public interface EduSubjectService extends IService<EduSubject> {
+
+}
---
+++ service/service_edu/src/main/java/com/atjh/eduService/service/impl/EduSubjectServiceImpl.java
@@ -0,0 +1,20 @@
+package com.atjh.eduService.service.impl;
+
+import com.atjh.eduService.entity.EduSubject;
+import com.atjh.eduService.mapper.EduSubjectMapper;
+import com.atjh.eduService.service.EduSubjectService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 课程科目 服务实现类
+ * </p>
+ *
+ * @author jahui
+ * @since 2021-11-23
+ */
+@Service
+public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubject> implements EduSubjectService {
+
+}
